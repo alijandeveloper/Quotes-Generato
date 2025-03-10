a3146a43d82985cdd1f6ed2035542684de922de3
Ali Jan <alijandeveloper15@gmail.com>
2025-03-10 08:01:53 -0700

    get Element script

---
+++ script.js
@@ -0,0 +1,8 @@
+/* script.js */
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const newQuoteBtn = document.getElementById("new-quote");
+const copyBtn = document.getElementById("copy-quote");
+const tweetBtn = document.getElementById("tweet-quote");
+const speakBtn = document.getElementById("speak-quote");
+
